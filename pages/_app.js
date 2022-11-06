2b28c2a39336d8629f6886063a4207c4c0806625
TheHamsterDog <shreshth@shreshthverma.me>
2022-11-06 11:03:01 +0530

    feat: added meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
-
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+    <Head>
+      <title>Shreshth Verma: </title>
+      <meta property="og:type" content='website'/> 
+      <meta property='og:image' content='/image.jpg'/>
+      <link rel="icon" href="/favicon.ico" />
+      <meta property="description" content='A Full Stack Developer, a perpetual student and an entrepreneur'/>
+      <meta property="og:title" content='Shreshth Verma'/>
+      <meta property="og:description" content='A Full Stack Developer, a perpetual student and an entrepreneur'/>
+      <meta property="og:url" content='https://www.shreshthverma.me'/>
+      <meta property="og:site_name" content='Shreshth Verma'/>
+      <meta property="og:locale" content='en_US'/>
+      <meta charSet='utf-8'/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </Head>
+    <Component {...pageProps} />
+  </div>
+  )
 }
 
 export default MyApp
